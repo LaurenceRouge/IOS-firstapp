b1af3eb62957f8a63ec98ecc1084b19bcf5e7b12
LaurenceRouge <laurence.rouge@hetic.net>
2017-11-27 11:10:12 +0100

    Initial Commit

---
+++ IOS-retest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-retest
+//
+//  Created by Laurence on 27/11/2017.
+//  Copyright © 2017 Hetic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
